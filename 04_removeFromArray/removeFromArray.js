3302fe9139ea0fcbb4a876de85b4f97c7df02fc9
neppur <191008508+neppur@users.noreply.github.com>
2025-01-30 18:44:20 +0100

    Finish all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(total, ...def) {
+    let minus = Array.from(def);
+
+    let result = total.filter(item => !minus.includes(item));
+
+    return result;
+    
 };
 
-// Do not edit below this line
+
+// Do not edit below this lineg
 module.exports = removeFromArray;
+
+
+// compare elements in array to elements outside
+// if elements in array equals element outside
+    // remove
+// else console log
