737475284e29df2912c87c6ce7d2ba0fd1780641
neppur <191008508+neppur@users.noreply.github.com>
2025-01-30 20:55:30 +0100

    Finish exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,39 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let total = 0;
+    let i = start;
+
+    if (i < -1 
+        || end < -1 
+        || Number.isInteger(i, end) === false 
+        || typeof end === 'string' 
+        || typeof i === 'string'
+        || typeof end === 'object' 
+        || typeof i === 'object'
+        // || Number.isNaN(i, end) === false
+        )
+        {
+        return 'ERROR';
+    }
+
+    else if (i > end) {
+        while (i > end) {
+            i--;
+            total += i;
+        }
+       return total + start;
+    }
+
+    while (i < end) {
+        i++;
+        total += i;
+    }
+    return total + start;
 
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
+// start increase 1     
+    // add to total
+// end on end
