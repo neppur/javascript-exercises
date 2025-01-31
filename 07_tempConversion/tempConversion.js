15c3de9fd0c7a36144533c42f326ae2ebe0bfa8b
neppur <191008508+neppur@users.noreply.github.com>
2025-01-31 14:33:10 +0100

    Finish exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celsius = (fahrenheit - 32) * 5 / 9;
+  // return celsius.toFixed(1);
+  return Math.round(celsius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let fahrenheit = celsius * 9/5 + 32;
+  // return fahrenheit.toFixed(1);
+  return Math.round(fahrenheit * 10) / 10;
 };
 
 // Do not edit below this line
@@ -9,3 +15,6 @@ module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+
+//  formel für von F zu C (32 °F − 32) × 5/9 = 0 °C
+// für C zu F (0 °C × 9/5) + 32 = 32 °F
